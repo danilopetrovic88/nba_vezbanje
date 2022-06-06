8d57a6aa7fdf50995e34aca5075ff23d57af736b
Danilo Petrovic <daniloljubica14@gmail.com>
2022-06-06 18:56:59 +0200

    PlayerTableSeeder, PlayerFactory, TeamTableSeeder, TeamFactory

--- database/seeders/TeamTableSeeder.php
+++ database/seeders/TeamTableSeeder.php
@@ -2,21 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\Team;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class TeamTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call([
-            TeamTableSeeder::class,
-            PlayerTableSeeder::class
-        ]);
+        Team::factory(30)->create();
     }
 }
